84bd7a6b5489fb6334d02f44eaef5d73e62e4e2c
Stas Tolmachev <stusik@yandex.ru>
2023-12-26 23:15:29 +0300

    first commit

---
+++ screens/FullPost.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+import { View, Alert } from 'react-native';
+import styled from 'styled-components/native';
+import { Loading } from '../components/Loading';
+
+const PostImage = styled.Image`
+  border-radius: 10px;
+  width: 100%;
+  height: 250px;
+  margin-bottom: 20px;
+`;
+
+const PostText = styled.Text`
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+export const FullPostScreen = ({ route, navigation }) => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [data, setData] = React.useState();
+  const { id, title } = route.params;
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      title,
+    });
+    axios
+      .get(`https://e94c61e55e3c332b.mokky.dev/posts/${id}`)
+
+      .then(({ data }) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        Alert.alert('Ошибка', 'Не удалось получить статью');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [id]);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Loading />
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ padding: 20 }}>
+      <PostImage source={{ uri: data?.imageUrl }} />
+      <PostText>{data?.text}</PostText>
+    </View>
+  );r
+};
